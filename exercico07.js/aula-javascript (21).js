efa88482998f4cc1bcd1832fe0c5d91250283324
Agatha-gif152 <0001250838@senaimgaluno.com.br>
2026-07-02 15:42:05 -0300

    Add files via upload

---
+++ exercico07.js/aula-javascript (21).js
@@ -0,0 +1,5 @@
+let base = Number(prompt("Digite a base do triângulo (em metros):"));
+let altura = Number(prompt("Digite a altura do triângulo (em metros):"));
+let area = (base * altura) / 2;
+
+console.log(`A área total do triângulo é de: ${area} m²`);
