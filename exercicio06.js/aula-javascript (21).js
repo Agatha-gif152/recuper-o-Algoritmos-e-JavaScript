c48b5c0d0cbdb18eb1d828a001f6e870bcf1271c
Agatha-gif152 <0001250838@senaimgaluno.com.br>
2026-07-02 15:32:59 -0300

    Update aula-javascript (21).js

--- exercicio06.js/aula-javascript (21).js
+++ exercicio06.js/aula-javascript (21).js
@@ -1,7 +1,5 @@
 let num1 = Number(prompt("Digite o primeiro número inteiro:"));
 let num2 = Number(prompt("Digite o segundo número inteiro:"));
-
-// Math.trunc extrai a parte inteira do resultado da divisão
 let quociente = Math.trunc(num1 / num2); 
 let resto = num1 % num2;
 
