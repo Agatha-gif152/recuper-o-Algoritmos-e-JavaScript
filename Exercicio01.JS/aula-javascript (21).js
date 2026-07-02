0a251d3fe950c326183867907d97cdbabe43ef06
Agatha-gif152 <0001250838@senaimgaluno.com.br>
2026-07-02 15:11:35 -0300

    Add files via upload

---
+++ Exercicio01.JS/aula-javascript (21).js
@@ -0,0 +1,6 @@
+let numero = Number(prompt("Digite um número real :"));
+let dobro = numero * 2;
+let triplo = numero * 3;
+console.log("O número informado foi :" + numero);
+console.log("O dobro de" + numero + " é:" + dobro);
+console.log("O trplo desse número é:" + triplo);
