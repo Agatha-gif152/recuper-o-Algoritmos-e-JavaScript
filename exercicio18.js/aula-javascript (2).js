2fe7b64c626b46b14b736fd81417692c4f131f81
Agatha-gif152 <0001250838@senaimgaluno.com.br>
2026-07-03 14:12:27 -0300

    Delete exercicio18.js/aula-javascript (2).js

--- exercicio18.js/aula-javascript (2).js
+++
@@ -1,8 +0,0 @@
-function verificarTriangulo(a, b, c) {
-    if (a < b + c && b < a + c && c < a + b) {
-        console.log("Os comprimentos formam um Triângulo Válido.");
-    } else {
-        console.log("Não é possível formar um Triângulo.");
-    }
-}
-verificarTriangulo(3, 4, 5);
