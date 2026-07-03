bb735b0506c4e4bb2c31909abdae4fec1025c075
Agatha-gif152 <0001250838@senaimgaluno.com.br>
2026-07-03 15:41:25 -0300

    Add files via upload

---
+++ exercicio50.js/aula-javascript (1).js
@@ -0,0 +1,6 @@
+const vetorSeis = [10, 20, 30, 40, 50, 60];
+let somaVetor = 0;
+for (let i = 0; i < vetorSeis.length; i++) {
+    somaVetor += vetorSeis[i];
+}
+console.log(`Soma do vetor: ${somaVetor}`);
