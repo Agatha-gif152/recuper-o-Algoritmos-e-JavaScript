0e68adc166ba1528da69e1f8bcc08eb072acf535
Agatha-gif152 <0001250838@senaimgaluno.com.br>
2026-07-03 13:59:41 -0300

    Add files via upload

---
+++ exercicio15.js/aula-javascript (1).js
@@ -0,0 +1,9 @@
+function analisarEmprestimo(salarioBruto, parcelaMensal) {
+    const limite = salarioBruto * 0.30;
+    if (parcelaMensal <= limite) {
+        console.log("Parecer: Aprovado");
+    } else {
+        console.log("Parecer: Negado");
+    }
+}
+analisarEmprestimo(3000, 800); // Exemplo de teste
