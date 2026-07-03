120c421382f410a4c635447b016988155efab88a
Agatha-gif152 <0001250838@senaimgaluno.com.br>
2026-07-03 15:47:45 -0300

    Add files via upload

---
+++ exercicio53.js/aula-javascript (1).js
@@ -0,0 +1,8 @@
+const vetorA = [2, 4, 6, 8, 10];
+const vetorB = [];
+
+for (let i = 0; i < vetorA.length; i++) {
+    vetorB[i] = vetorA[i] * 2;
+}
+console.log("Vetor A:", vetorA);
+console.log("Vetor B (Dobro):", vetorB);
