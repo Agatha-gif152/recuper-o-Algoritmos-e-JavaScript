3ac18228b6b0d3180eb19965d2f8dc5f78453af2
Agatha-gif152 <0001250838@senaimgaluno.com.br>
2026-07-03 15:41:06 -0300

    Add files via upload

---
+++ exercicio49.js/aula-javascript (1).js
@@ -0,0 +1,4 @@
+const nomes = ["Ana", "Bruno", "Carlos", "Diana", "Eduardo"]; // Simulando preenchimento
+for (let i = 0; i < nomes.length; i++) {
+    console.log(`Usuário ${i + 1}: ${nomes[i]}`);
+}
